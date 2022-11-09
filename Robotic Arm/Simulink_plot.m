ae9dd26033a7ef5b28b5ec223fcaf2d175bf13bc
2571964s <2571964s@student.gla.ac.uk>
2022-11-09 17:18:00 +0000

    Added Simulink code for plotting

---
+++ Robotic Arm/Simulink_plot.m
@@ -0,0 +1,16 @@
+
+figure(1)										% define figure window number
+clf												% clear figure
+hold on
+% grid on
+
+plot(tout, theta_M_out) % theta M
+plot(tout,theta_F_out) % theta F
+plot(tout,theta_Fdot_out) % theta F dot
+
+%       theta M         theta F         theta F dot
+legend('Motor angle', 'Forearm angle', 'Speed of forearm')
+xlabel('Time (s)')
+ylabel('Radians')
+
+hold off
