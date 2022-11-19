93993bef6fed06518d79c45b79431f7019262b0a
Jalal Sayed <mrjalalsayed@gmail.com>
2022-11-19 15:40:28 +0000

    Added part 1 and 2 folders

--- Robotic Arm/rk4int.m
+++
@@ -1,9 +0,0 @@
-function x = rk4int(modelname, h, x, Va)
-% This function performs one 4th Order Runge-Kutta integration step
-
-k1 = h*feval(modelname, x, Va);		        % evaluate derivative k1
-k2 = h*feval(modelname, x+k1/2, Va);	    % evaluate derivative k2
-k3 = h*feval(modelname, x+k2/2, Va);	    % evaluate derivative k3
-k4 = h*feval(modelname, x+k3, Va);		    % evaluate derivative k4
-
-x = x + (k1 + 2*k2 + 2*k3 + k4)/6;		    % averaged output
